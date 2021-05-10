8a4dfe5ded61489bc19803c9d2360e836d4c37e3
viki-lar <viki.lar@yandex.ru>
2021-05-11 00:11:19 +0400

    added: slider

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+
+    // Optional parameters
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-button__next',
+        prevEl: '.slider-button__prev',
+    },
+
+});
